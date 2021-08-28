d41edca501fc2a22ca5099de56275b2d5824987b
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-08-28 12:57:11 +0300

    feat

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ const { createUser, logout, login } = require('../controllers/users');
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
   }).unknown(true),
 }), createUser);
 
