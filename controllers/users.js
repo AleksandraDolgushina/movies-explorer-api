142e7a30649bf6bc0b4c1f95447708e213a9c135
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-08-29 15:24:06 +0300

    feat

--- controllers/users.js
+++ controllers/users.js
@@ -80,6 +80,7 @@ module.exports.login = (req, res, next) => {
         _id: user._id,
         email: user.email,
         name: user.name,
+        token: user.token
       });
     })
     .catch(() => {
